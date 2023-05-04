4441c55bbf06bd8faf1aa0cf2b0291a44b8c5bfd
valmur prado <pradovalmur@gmail.com>
2023-05-04 16:27:00 +0000

    first commit for rust4data

---
+++ rust4data/target/debug/deps/rust4data-092f2c4375473e6b.d
@@ -0,0 +1,5 @@
+/home/ubuntu/rust4data/rust4data/target/debug/deps/rust4data-092f2c4375473e6b: src/main.rs
+
+/home/ubuntu/rust4data/rust4data/target/debug/deps/rust4data-092f2c4375473e6b.d: src/main.rs
+
+src/main.rs:
---
+++ rust4data/target/debug/rust4data.d
@@ -0,0 +1,1 @@
+/home/ubuntu/rust4data/rust4data/target/debug/rust4data: /home/ubuntu/rust4data/rust4data/src/main.rs
